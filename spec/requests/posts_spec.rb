09e8e54d7ddd7368765f59574669da22e82a7250
David Corking <dmc-source@dcorking.com>
2020-10-16 14:54:49 +0100

    Test POST /posts

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -12,4 +12,29 @@ RSpec.describe "Posts", type: :request do
       expect(response.body).to eq '[]'
     end
   end
+
+  describe "POST /posts" do
+    it 'succeeds' do
+      post posts_path, params: { post: { title: 'Hello world!' } }
+      expect(response).to have_http_status(201)
+    end
+
+    it 'adds a persistent post' do
+      expect do
+        post posts_path, params: { post: { title: 'Hello world!' } }
+      end.to change {Post.count}.by(1)
+      expect(Post.last.title).to eq('Hello world!')
+    end
+
+    it 'returns the object and integer id' do
+      post posts_path, params: { post: { title: 'Hello world!' } }
+      expect(JSON.parse(response.body)). to match(
+                                    'id' => an_instance_of(Integer),
+                                    'title' => 'Hello world!',
+                                    'body' => nil,
+                                    'valid_from' => nil,
+                                    'expires'=> nil,
+                                )
+    end
+  end
 end
